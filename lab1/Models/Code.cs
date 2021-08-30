fd698ace50b4e2c6f9538d49a3c8b8961ea2b8e5
Niktia <nmgorovenko@yandex.ru>
2021-08-30 23:49:18 +0700

    MVVM is implemented

---
+++ lab1/Models/Code.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace lab1.Models
+{
+    class Code : INotifyPropertyChanged
+    {
+
+
+        string inputString;
+        string outputString;
+
+        public Code()
+        {
+            this.inputString = "";
+            this.outputString = "";
+        }
+
+        public string InputString
+        {
+            get { return inputString; }
+            set
+            {
+                inputString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OutputString
+        {
+            get { return outputString; }
+            set
+            {
+                outputString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // алгоритм шифрования
+        public void Encrypt()
+        {
+            OutputString = InputString + "  \nEncrypted";
+            
+        }
+
+        public void Decrypt()
+        {
+            InputString = "Нас раскодировали";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ lab1/ViewModels/MainWIndowViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using lab1.Models;
+using lab1.Commands;
+using System.Runtime.CompilerServices;
+
+namespace lab1.ViewModels
+{
+    class MainWIndowViewModel : INotifyPropertyChanged
+    {
+
+        private Code codeEncrypterDecrypter;
+
+
+        // Command to append element
+        private RelayCommand encryptCommand;
+        public RelayCommand EncryptCommand 
+        {
+            get
+            {
+                return encryptCommand ?? 
+                    (encryptCommand = new RelayCommand(obj =>
+                    {
+                        codeEncrypterDecrypter.Encrypt();
+                    }));
+            }
+        }
+        private RelayCommand decryptCommand;
+        public RelayCommand DecryptCommand
+        {
+            get
+            {
+                return decryptCommand ??
+                    (decryptCommand = new RelayCommand(obj =>
+                    {
+                        codeEncrypterDecrypter.Decrypt();
+                    }));
+            }
+        }
+        public Code CodeEncrypterDecrypter
+        { 
+            get { return codeEncrypterDecrypter; }
+            set
+            {
+                codeEncrypterDecrypter = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainWIndowViewModel()
+        {
+            this.codeEncrypterDecrypter = new Code();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
